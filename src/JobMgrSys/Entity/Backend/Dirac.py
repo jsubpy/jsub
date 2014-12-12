44de802899d9e659e4ea15c3804135a993a9f2f0
Zhao Xianghu <zhaoxh@ihep.ac.cn>
2014-12-12 18:07:39 +0800

    Add DIRAC job group

--- src/JobMgrSys/Entity/Backend/Dirac.py
+++ src/JobMgrSys/Entity/Backend/Dirac.py
@@ -1,3 +1,6 @@
+import string
+import random
+
 from ...Core.Backend import Backend
 
 from DIRAC.Core.Base import Script
@@ -8,7 +11,8 @@ from DIRAC.Interfaces.API.Job import Job
 
 class Dirac(Backend):
     def __init__(self):
-        self.__site = ''
+        self.__site = []
+        self.__auto_job_group = False
 
     def submit(self, param):
         job_name = param['job_name']
@@ -23,8 +27,16 @@ class Dirac(Backend):
         j.setName(job_name)
         j.setInputSandbox(input)
         j.setOutputSandbox(output)
+
         if 'input_data' in param:
             j.setInputData(param['input_data'])
+        if 'output_data' in param:
+            j.setOutputData(param['output_data'])
+
+        if self.__job_group:
+            j.setJobGroup(self.__job_group)
+        elif self.__auto_job_group:
+            j.setJobGroup('nogroup_' + ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(6)))
 
         dirac = GridDirac()
         result = dirac.submit(j)
@@ -38,3 +50,9 @@ class Dirac(Backend):
 
     def setSite(self, site):
         self.__site = site
+
+    def setJobGroup(self, job_group):
+        self.__job_group = job_group
+
+    def setAutoJobGroup(self, auto_job_group):
+        self.__auto_job_group = auto_job_group
