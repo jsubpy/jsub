7202ee86c3b5cd92f5c72bbb33391d4661865b6b
Zhao Xianghu <zhaoxh@ihep.ac.cn>
2017-03-01 11:25:45 +0800

    "last" in sequence splitter is optional

--- jsub/exts/splitter/sequence.py
+++ jsub/exts/splitter/sequence.py
@@ -1,18 +1,13 @@
-from jsub.error import SplitterParamError
-
 class Sequence(object):
     def __init__(self, param):
-        if 'last' not in param:
-            raise SplitterParamError('Parameter not found: "last"')
-
         self.__first = param.get('first', 1)
-        self.__last  = param['last']
+        self.__last  = param.get('last')
         self.__step  = param.get('step',  1)
 
         self.__value = self.__first
 
     def next(self):
-        if self.__value > self.__last:
+        if self.__last and self.__value > self.__last:
             raise StopIteration
         value = self.__value
         self.__value += self.__step
